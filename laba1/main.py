9caf1d352b866dfdc7bac54e0332c39d24e8726f
Evgeniy Borisov <borisovjenya33@gmail.com>
2023-02-07 16:05:24 +0500

    зачтена1

--- laba1/main.py
+++ laba1/main.py
@@ -1,7 +1,7 @@
 import string as str
 import math as m
 
-print('Для подсчета мощности пароля будет использоваться латинский алфавит')
+print('Для анлиза надежности пароля будет использоваться латинский алфавит')
 password = input('Ведите пароль: ')
 speed_password = int(input('Введите скорость подбора пароля: '))
 count_failed = int(input('Введите количество попыток подбора пароля после которого будет сделана пауза: '))
@@ -19,40 +19,45 @@ for i in range(len(password)):
     if digit != True:
         if password[i].isdigit():
             digit = True
-            power += 10
-    if lower != True:
-        if password[i].islower():
-            lower = True
-            power += 26
-    if upper != True:
-        if password[i].isupper():
-            upper = True
-            power += 26
-    if space !=True:
-        if password[i].isspace():
-            space = True
-            power +=1
-    if spec_char != True:
-        for j in range(len(special_characters)):
-            if password[i] == special_characters[j]:
-                spec_char = True
-                power += 32
-                break
-        j = 0
+power += 10
+if lower != True:
+    if password[i].islower():
+        lower = True
+power += 26
+if upper != True:
+    if password[i].isupper():
+        upper = True
+power += 26
+if space != True:
+    if password[i].isspace():
+        space = True
+power += 1
+if spec_char != True:
+    for j in range(len(special_characters)):
+        if password[i] == special_characters[j]:
+            spec_char = True
+            power += 32
+            break
+    j = 0
 
 password_length = len(password)
-password_power = pow(power,password_length)
-password_time_seconds = (password_power/speed_password) + (password_power/count_failed)*pause_time
+password_power = pow(power, password_length)
+password_time_without_pause = int(password_power / speed_password)
+password_time_seconds = password_time_without_pause + (password_power / count_failed) * pause_time
 
-years = int(password_time_seconds//60//60//24//365)
-mounths = int((password_time_seconds//60//60//24//30)%12)
-days = int((password_time_seconds//60//60//24)%30)
-hours = int((password_time_seconds//60//60)%24)
-minutes = int((password_time_seconds//60)%60)
-seconds = m.floor(password_time_seconds%60)
+if password_power % count_failed == 0:
+    password_time_seconds -= pause_time
 
+years = int(password_time_seconds // 60 // 60 // 24 // 365)
+mounths = int((password_time_seconds // 60 // 60 // 24 // 30) % 12)
+days = int((password_time_seconds // 60 // 60 // 24) % 30)
+hours = int((password_time_seconds // 60 // 60) % 24)
+minutes = int((password_time_seconds // 60) % 60)
+seconds = m.floor(password_time_seconds % 60)
 
 print('Длина пароля = ', password_length, 'символам')
 print('Мощность алфавита = ', power)
 print('Мощность пространства = ', password_power)
-print('Подбор пароля займет ', years, ' лет ', mounths, ' месяцев ', days, ' дней ', hours, ' часов ', minutes, ' минут ', seconds, 'секунд')
+print('Время подбора пароля без учета пауз ', password_time_without_pause, ' секунд')
+print('Подбор пароля займет ', years, ' лет ', mounths, ' месяцев ', days, ' дней ', hours, ' часов ', minutes,
+      ' минут ', seconds, 'секунд')
